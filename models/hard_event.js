cb99b573a9e0d688e7ceda80faab77ee1a8ded4c
Sanat Deshpande <sanatdeshpandejhu@gmail.com>
2017-09-16 18:15:34 -0400

    Added logic for hard event buffers

--- models/hard_event.js
+++ models/hard_event.js
@@ -15,17 +15,40 @@ class HardEvent extends BaseEvent {
     }
 
     getFunction(start, end) {
-      const CONST = 1;
-      if (end < this.startTime || start > (this.endTime)) {
-        return 0;
-      }
-      if (start < this.startTime) {
-        start = this.startTime;
-      }
-      if (end > this.endTime) {
-        end = this.endTime;
-      }
-      return (end - start) * CONST
+      const BUFFER = 15 * 60 * 1000;
+      var rectangle = getRectangleArea(start, end);
+      var triangleOne = getTriangleArea(start, end, this.startTime - BUFFER, this.startTime);
+      var triangleTwo = getTriangleArea(start, end, this.endTime, this.endTime + BUFFER);
+      return rectangle + triangleOne + triangleTwo;
+    }
+
+    getRectangleArea(start, end) {
+        const CONST = 1;
+        if (end < this.startTime || start > this.endTime) {
+          return 0;
+        }
+        if (start < this.startTime) {
+          start = this.startTime;
+        }
+        if (end > this.endTime) {
+          end = this.endTime;
+        }
+        return (end - start) * CONST
+    }
+
+    getTriangleArea(startFlex, endFlex, startHard, endHard) {
+        const CONST = 1;
+        if (endFlex < startHard || startFlex > endHard) {
+          return 0;
+        }
+        if (startFlex < startHard) {
+          startFlex = startHard;
+        }
+        if (endFlex > endHard) {
+          endFlex = endHard;
+        }
+        var height = CONST * (endFlex - startFlex) / (endHard - startHard);
+        return (endFlex - startFlex) * height / 2;
     }
 }
 
