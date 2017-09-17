19bd459bd8eb473bec47a5e762d829a9b2da290a
Sanat Deshpande <sanatdeshpandejhu@gmail.com>
2017-09-16 20:16:39 -0400

    Fixed more stuff in schedule builder

--- controllers/schedule_builder_controller.js
+++ controllers/schedule_builder_controller.js
@@ -1,34 +1,55 @@
-var HardEvent = require("./models/hard_event.js");
-var Constraint = require("./models/constraint.js");
-var FlexEvent = require("./models/flex_event.js");
-var Schedule = require("./models/schedule.js");
+var HardEvent = require("../models/hard_event.js");
+var Constraint = require("../models/constraint.js");
+var FlexEvent = require("../models/flex_event.js");
+var Schedule = require("../models/schedule.js");
 var moment = require('moment');
 
+class ScheduleBuilderController {
+    constructor(hardEvents, flexEvents, constraints) {
+        this.hardEvents = hardEvents;
+        this.flexEvents = flexEvents;
+        this.constraints = constraints;
+    }
+    /**
+     * build best schedules given hard, flex and constraint events.
+     * @param hardEvents, @param flexEvents, @param constraints arrays of each type of event;
+     */
+    buildSchedules() {
+      var scheduledEvents = this.hardEvents.slice(0); //gets all hardEvents
+      this.constraints.forEach(function(constraint) {
+        scheduledEvents.push(constraint);
+      })
 
-/**
- * build best schedules given hard, flex and constraint events.
- * @param hardEvents, @param flexEvents, @param constraints arrays of each type of event;
- */
-function buildSchedules(hardEvents, flexEvents, constraints) {
-  var SheduledEvents = hardEvents.slice(0);
-  constraints.forEach(function(constraint) {
-    SheduledEvents.push(constraint);
-  })
+      var rankedFlexEvents = this.rank(this.flexEvents);
+    }
 
-  var rankedFlexEvents = rank(flexEvents);
+    /**
+     * Fits one ranked event into a slot
+     * @param scheduledEvents @param flexEvent
+     * @return one hard event (a fit flex event)
+     */
+    fitFlexedEvents(scheduledEvents, flexEvent) {
+        var min = -1;
+        for (var j = 0; j < scheduledEvents.length; j++) {
+            var temp = 0;
+            temp += scheduledEvents[j].getFunction();
+        }
+    }
+
+    rank(flexEvents) {
+      var rankedEvents = [];
+      flexEvents.forEach(function(event) {
+        var date = new Date(moment().toDate() - event.completeBy);
+        var rank = (event.length - (date.getHours() / 24 * (Math.ceil(event.constraint / 3) + 1)));
+        rankedEvents.push({
+          "event": event,
+          "rank": rank
+        })
+      })
+      return rankedEvents;
+    }
 }
 
-function rank(flexEvents) {
-  var rankedEvents = [];
-  flexEvents.forEach(function(event) {
-    rank = event.length - (Date.getHours(moment().toDate() - event.completeBy)) / 24 * (Math.ceil(event.constraint / 3) + 1)
-    rankedEvents.push({
-      "event": event;
-      "rank": rank;
-    })
-  })
 
-  rankedEvents.sort(function(a,b) {
-    return a.rank - b.rank;
-  });
-}
+
+module.exports = ScheduleBuilderController;
